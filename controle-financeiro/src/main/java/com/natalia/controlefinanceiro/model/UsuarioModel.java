77d015a31c661a0beb280657d021e1f38fb293f4
Natalia Nascimento <nataliasanntos161@gmail.com>
2025-12-16 15:47:24 -0300

    First commit

---
+++ controle-financeiro/src/main/java/com/natalia/controlefinanceiro/model/UsuarioModel.java
@@ -0,0 +1,4 @@
+package com.natalia.controlefinanceiro.model;
+
+public class UsuarioModel {
+}
---
+++ controle-financeiro/src/main/java/com/natalia/controlefinanceiro/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package com.natalia.controlefinanceiro.repository;
+
+public class UsuarioRepository {
+}
