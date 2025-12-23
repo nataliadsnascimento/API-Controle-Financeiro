9db4b356b1b2d0e5234794da4dac4d171093cd99
Natalia Nascimento <nataliasanntos161@gmail.com>
2025-12-23 19:53:23 -0300

    feat: implement security base infrastructure with JWT

--- controle-financeiro/src/main/java/com/natalia/controlefinanceiro/model/UsuarioModel.java
+++ controle-financeiro/src/main/java/com/natalia/controlefinanceiro/model/UsuarioModel.java
@@ -2,10 +2,15 @@ package com.natalia.controlefinanceiro.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
-public class UsuarioModel {
+public class UsuarioModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ID automático (1, 2...)
@@ -13,4 +18,40 @@ public class UsuarioModel {
     private String nome;
     private String email;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return  List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
---
+++ controle-financeiro/src/main/java/com/natalia/controlefinanceiro/service/TokenService.java
@@ -0,0 +1,34 @@
+// Local: .../service/TokenService.java
+package com.natalia.controlefinanceiro.service;
+
+import com.natalia.controlefinanceiro.model.UsuarioModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret:secret-key-H3ep4@w!73rxz}")
+    private String secret;
+
+    public String gerarToken(UsuarioModel usuario) {
+        return Jwts.builder()
+                .setIssuer("API Controle Financeiro")
+                .setSubject(usuario.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 dia
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String getSubject(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
